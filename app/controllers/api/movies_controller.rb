2b0c41f797d886a0fb602b4f07a4d2b2ed136999
Daniel Williams <dlw1ll27@gmail.com>
2021-02-25 15:11:48 -0800

    adds finished exercise 1 and 3. Still missing exercise 2

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,8 +1,9 @@
 class Api::MoviesController < ApplicationController
   require "http"
+  # @response = HTTP.get("http://127.0.0.1:3000/api/get_movies")
 
   def get_all_movies
-    @all_movies = Movie.all
+    @all_movies = Movie.select([:id, :title])
     render "all_movies.json.jb"
   end
 
