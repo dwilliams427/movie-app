6ddbaa6122990c78722b7eddaa22b3402195da75
Daniel Williams <dlw1ll27@gmail.com>
2021-02-25 14:38:54 -0800

    adds working movies routes

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,2 +1,13 @@
 class Api::MoviesController < ApplicationController
+  require "http"
+
+  def get_all_movies
+    @all_movies = Movie.all
+    render "all_movies.json.jb"
+  end
+
+  def get_movie_by_id
+    @movie = Movie.find_by(id: 3)
+    render "first_movie.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/get_actor" => "actors#get_actor_by_id"
+    get "/get_movie" => "movies#get_movie_by_id"
+    get "/get_movies" => "movies#get_all_movies"
   end
 end
